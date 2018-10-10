73da22e73f592d6d1808f6a1faae7c42a4c34ae9
Sravan Kumar <sravanwithu14@gmail.com>
2018-10-10 22:35:14 +0530

    Add files via upload

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
+import header from './components/header';
+import employeeDetails from './components/employeeDetails';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+const Enzyme = require('enzyme');
+const EnzymeAdapter = require('enzyme-adapter-react-15');
+
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+describe('header', () => {
+  /* it('should render app component', () => {
+    const appComponent = shallow(
+      <App className='App' />
+      );
+      expect(appComponent).toMatchSnapshot();
+  }); */
+  it('should be defined', () => {
+    expect(employeeDetails).toBeDefined();
+  });
+  it('should render correctly', () => {
+    const tree = shallow(
+      <employeeDetails className='employee-data' />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  it('should be defined', () => {
+    expect(header).toBeDefined();
+  });
+  it('should render correctly', () => {
+    const tree = shallow(
+      <header className='header-container' />
+    );
+    expect(tree).toMatchSnapshot();
+  });
+ });
